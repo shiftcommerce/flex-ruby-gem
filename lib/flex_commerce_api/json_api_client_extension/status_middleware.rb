c1c8ef97cdc3b554658a96d298306e8016150c1e
Gary Taylor <gary.taylor@hismessages.com>
2015-08-07 19:24:35 +0100

    request environment is now duplicated as it was being modified - for exception handling

--- lib/flex_commerce_api/json_api_client_extension/status_middleware.rb
+++ lib/flex_commerce_api/json_api_client_extension/status_middleware.rb
@@ -2,13 +2,14 @@ module FlexCommerceApi
   module JsonApiClientExtension
     class StatusMiddleware < Faraday::Middleware
       def call(environment)
+        request_env = environment.dup
         @app.call(environment).on_complete do |env|
-          handle_status(env[:status], env, environment)
+          handle_status(env[:status], env, request_env)
 
           # look for meta[:status]
           if env[:body].is_a?(Hash)
             code = env[:body].fetch("meta", {}).fetch("status", 200).to_i
-            handle_status(code, env, environment)
+            handle_status(code, env, request_env)
           end
         end
       rescue Faraday::ConnectionFailed, Faraday::TimeoutError
