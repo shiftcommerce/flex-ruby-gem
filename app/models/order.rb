a1790df99422cb15776ba1c5baa05a29af333ae8
Gary Taylor <gary.taylor@hismessages.com>
2015-11-04 08:39:06 +0000

    Added order model - not yet specified as API is in progress

---
+++ app/models/order.rb
@@ -0,0 +1,13 @@
+require "flex_commerce_api/api_base"
+module FlexCommerce
+  #
+  # A flex commerce Order model
+  #
+  # This model provides access to the flex commerce order
+  #
+  #
+  #
+  class Order < FlexCommerceApi::ApiBase
+
+  end
+end
