971b0169c4c048acc7e101cfb1b4fcd1f78f3e89
Gary Taylor <gary.taylor@hismessages.com>
2015-11-17 08:09:17 +0000

    Addresses and order shipping method

---
+++ lib/flex/commerce/api.rb
@@ -0,0 +1,9 @@
+require "flex/commerce/api/version"
+
+module Flex
+  module Commerce
+    module Api
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/flex/commerce/api/version.rb
@@ -0,0 +1,7 @@
+module Flex
+  module Commerce
+    module Api
+      VERSION = "0.1.0"
+    end
+  end
+end
