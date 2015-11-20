7fbf4da11a64a4bc833465b63346af742080c7e6
Gary Taylor <gary.taylor@hismessages.com>
2015-11-20 15:55:57 +0000

    Added asset file mode

---
+++ app/models/asset_file.rb
@@ -0,0 +1,13 @@
+require "flex_commerce_api/api_base"
+module FlexCommerce
+  #
+  # A flex commerce asset file model
+  #
+  # This model provides access to the flex asset file
+  #
+  # It is used much like an active record model.
+  #
+  class AssetFile < FlexCommerceApi::ApiBase
+
+  end
+end
--- lib/flex_commerce.rb
+++ lib/flex_commerce.rb
@@ -21,4 +21,5 @@ module FlexCommerce
   autoload :Order, File.join(gem_root, "app", "models", "order")
   autoload :TemplateDefinition, File.join(gem_root, "app", "models", "template_definition")
   autoload :SearchSuggestion, File.join(gem_root, "app", "models", "search_suggestion")
+  autoload :AssetFile, File.join(gem_root, "app", "models", "asset_file")
 end
