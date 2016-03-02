dd1ad340591660b33e835310c627c83a3cb47d84
Ryan Townsend <ryan@ryantownsend.co.uk>
2016-03-02 15:22:46 +0000

    Fixed funky indents

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,5 @@
 require "flex_commerce_api/api_base"
-  module FlexCommerce
-    class Email < FlexCommerceApi::ApiBase
-    end
+module FlexCommerce
+  class Email < FlexCommerceApi::ApiBase
+  end
 end
