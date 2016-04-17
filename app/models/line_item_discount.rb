c299447bfc53a3ba9bce9d657e9dc4acc069589c
Ryan Townsend <ryan@ryantownsend.co.uk>
2016-04-17 17:40:12 +0100

    Ensure LineItemDiscount model has correct Promotion model namespace

--- app/models/line_item_discount.rb
+++ app/models/line_item_discount.rb
@@ -2,6 +2,6 @@ require "flex_commerce_api/api_base"
 
 module FlexCommerce
   class LineItemDiscount < FlexCommerceApi::ApiBase
-    has_one :promotion
+    has_one :promotion, class_name: "::FlexCommerce::Promotion"
   end
 end
--- lib/flex_commerce_api/version.rb
+++ lib/flex_commerce_api/version.rb
@@ -1,4 +1,4 @@
 module FlexCommerceApi
-  VERSION = "0.3.28"
+  VERSION = "0.3.29"
   API_VERSION = "v1"
 end
