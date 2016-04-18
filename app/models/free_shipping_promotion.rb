5e47879c0ab5cfd4053233f4f48f9d5ff04cb4dd
laffy <matt.otb@gmail.com>
2016-04-18 15:17:07 +0100

    Update details

--- app/models/free_shipping_promotion.rb
+++ app/models/free_shipping_promotion.rb
@@ -1,7 +1,7 @@
 require "flex_commerce_api/api_base"
 module FlexCommerce
   #
-  # A flex commerce discount summary model
+  # A flex Free Shipping Promotion summary model
   #
   # This model is used by the Cart model as an association so is not
   # usable directly on the API as there is no corresponding URL
