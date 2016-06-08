c2d44a99fe62cf8bd34914e08576d6826d367be0
Mufudzi Masaire <mufudzi.masaire@flexcommerce.com>
2016-06-08 11:15:27 +0100

    review updates

--- lib/flex_commerce_api/api_base.rb
+++ lib/flex_commerce_api/api_base.rb
@@ -112,20 +112,20 @@ module FlexCommerceApi
     end
 
     def meta_attribute(key)
-      begin
-        return self.send(key) if RELATED_META_RESOURCES.include?(attributes[:meta_attributes][key][:data_type])
+      return nil unless meta = attributes[:meta_attributes][key]
+      if RELATED_META_RESOURCES.include?(meta[:data_type])
+        self.send(key)
+      else
         attributes[:meta_attributes][key][:value]
-      rescue
-        nil
       end
     end
 
     def template_attribute(key)
-      begin
-        return self.send("template_#{key}") if RELATED_META_RESOURCES.include?(attributes[:template_attributes][key][:data_type])
+      return nil unless template_meta = attributes[:template_attributes][key]
+      if RELATED_META_RESOURCES.include?(template_meta[:data_type])
+        self.send("template_#{key}")
+      else
         attributes[:template_attributes][key][:value]
-      rescue
-        nil
       end
     end
 
