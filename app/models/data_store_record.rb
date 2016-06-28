2aab9e21c59253c022905a861b4b1c3cbe30f457
Keith McDonnell <keith@dancingtext.com>
2016-06-28 15:35:18 +0100

    Fix paths

--- app/models/data_store_record.rb
+++ app/models/data_store_record.rb
@@ -3,7 +3,7 @@ require "flex_commerce_api/api_base"
 module FlexCommerce
   class DataStoreRecord < FlexCommerceApi::ApiBase
     def self.table_name
-      'generic_data_store/record'
+      'generic_data_store/records'
     end
   end
 end
--- app/models/data_store_type.rb
+++ app/models/data_store_type.rb
@@ -3,7 +3,7 @@ require "flex_commerce_api/api_base"
 module FlexCommerce
   class DataStoreType < FlexCommerceApi::ApiBase
     def self.table_name
-      'generic_data_store/type'
+      'generic_data_store/record_types'
     end
   end
 end
