77ae65ea93d89de14ba447d717c3012558e51c35
garytaylor <gary.taylor@hismessages.com>
2016-07-11 10:07:55 +0100

    The redirect api model is now simplified to no longer use custom (#66)
    * The redirect api model is now simplified to no longer use custom endpoint (as it is a filter)
    
    * The redirect api model is now simplified to no longer use custom endpoint (as it is a filter)
    
    * The redirect api model is now simplified to no longer use custom endpoint (as it is a filter)

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -7,11 +7,8 @@ module FlexCommerce
   #
   class Redirect < FlexCommerceApi::ApiBase
 
-    # abstraction to ensure redirects can be loaded easily
-    collection_endpoint :matches, request_method: :get
-
     def self.find_by_resource(source_type: nil, source_slug: nil, source_path: )
-      matches(filter: { source_type: source_type, source_slug: source_slug, source_path: source_path }).first
+      where({type_slug_path: { source_type: source_type, source_slug: source_slug, source_path: source_path }}).first
     end
   end
 end
