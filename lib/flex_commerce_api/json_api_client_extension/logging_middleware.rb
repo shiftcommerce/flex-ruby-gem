7ee3eb7e52eac9a992d716e44db2d1d7c3a78040
Gary Taylor <gary.taylor@hismessages.com>
2016-09-01 09:20:07 +0100

    Minor fix to logger which can cause errors due to encoding.  No need to convert to JSON it is already a JSON string anyway

--- lib/flex_commerce_api/json_api_client_extension/logging_middleware.rb
+++ lib/flex_commerce_api/json_api_client_extension/logging_middleware.rb
@@ -15,7 +15,7 @@ module FlexCommerceApi
         self.request_id += 1
         @app.call(env).on_complete do |response_env|
           logger.debug("FlexApi::Request  id #{request_id} #{env.method.to_s.upcase} #{env.url} started at #{Time.now}")
-          logger.debug("\t >>> #{env[:request_body].to_json}") unless  env[:request_body].nil? || env[:request_body].empty?
+          logger.debug("\t >>> #{env[:request_body]}") unless  env[:request_body].nil? || env[:request_body].empty?
           logger.debug("\t <<< (#{env[:status]}) #{env.body}")
         end
       end
