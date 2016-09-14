7b9ffa57292a1b878d4eaf2da6e2540d0944d4cd
Gary Taylor <gary.taylor@hismessages.com>
2016-09-14 16:57:35 +0100

    Added a schema generator as I am gonna be doing a lot of them

--- spec/e2e/addresses_api_e2e_spec.rb
+++ spec/e2e/addresses_api_e2e_spec.rb
@@ -1,8 +1,43 @@
 require "e2e_spec_helper"
-RSpec.describe "Addresses API end to end spec" do
-  it_should_behave_like "crud endpoints" do
-    let(:base_attributes) { attributes_for(:address) }
-    let(:attributes_to_update) { { first_name: "#{base_attributes[:first_name]}CHANGED" } }
-    let(:model) { FlexCommerce::Address }
+RSpec.describe "Addresses API end to end spec", vcr: true do
+  let(:model) { FlexCommerce::Address }
+  context "#create" do
+    it "should persist when valid attributes are used" do |example|
+      subject = model.create first_name: "First",
+                   middle_names: "middle",
+                   last_name: "last",
+                   address_line_1: "Address line 1",
+                   address_line_2: "Address line 2",
+                   address_line_3: "Address line 3",
+                   city: "City",
+                   state: "State",
+                   postcode: "Postcode",
+                   country: "UK"
+      expect(subject).to be_persisted
+      expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
+
+    end
+  end
+  context "#show" do
+    it "should match the schema"
+    it "should have the correct values for the attributes"
+    it "should have no compound documents if not requested using include"
+    context "customer account relationship" do
+      it "should have links to the customer account that are accessible"
+      it "should have compound documents when requested"
+    end
+
+  end
+  context "#index" do
+
+  end
+  context "#archive" do
+
+  end
+  context "#unarchive" do
+
+  end
+  context "#destroy" do
+
   end
 end
