95d2765672dfee26f2deb1c8f420485751cf45ad
Gary Taylor <gary.taylor@hismessages.com>
2016-09-14 16:57:35 +0100

    Added a schema generator as I am gonna be doing a lot of them

--- spec/e2e_spec_helper.rb
+++ spec/e2e_spec_helper.rb
@@ -3,6 +3,7 @@ Dotenv.load
 require "spec_helper"
 require "flex_commerce_api"
 require "active_support/tagged_logging"
+require "json_matchers/rspec"
 root = File.expand_path("../", __dir__)
 Dir[File.join root, "spec/support_e2e/**/*.rb"].sort.each { |f| require f }
 raise "API_URL, API_ACCOUNT and API_KEY must be set in your environment" unless ENV.key?("API_URL") && ENV.key?("API_ACCOUNT") && ENV.key?("API_KEY")
