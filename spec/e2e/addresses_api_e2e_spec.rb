193dae414105fb75e36e4ee1e36138235a94ab88
Gary Taylor <gary.taylor@hismessages.com>
2016-09-15 12:39:05 +0100

    Added more schemas - still proving concept on addresses

--- spec/e2e/addresses_api_e2e_spec.rb
+++ spec/e2e/addresses_api_e2e_spec.rb
@@ -1,7 +1,17 @@
 require "e2e_spec_helper"
 RSpec.describe "Addresses API end to end spec", vcr: true do
+  _created_id = nil
+  _created_customer_account_id = nil
   let(:model) { FlexCommerce::Address }
+  let(:created_id) { _created_id }
+  let(:created_customer_account_id) { _created_customer_account_id }
   context "#create" do
+    before(:context) do
+      VCR.use_cassette "temp" do
+        _created_customer_account_id = FlexCommerce::CustomerAccount.create!(email: "testaccount#{Time.now.to_f}@domain.com", reference: "ref_#{Time.now.to_f}", password: "12345test67890").id
+        http_request_tracker.clear
+      end
+    end
     it "should persist when valid attributes are used" do |example|
       subject = model.create first_name: "First",
                    middle_names: "middle",
@@ -12,9 +22,12 @@ RSpec.describe "Addresses API end to end spec", vcr: true do
                    city: "City",
                    state: "State",
                    postcode: "Postcode",
-                   country: "UK"
+                   country: "UK",
+                   customer_account_id: created_customer_account_id
       expect(subject).to be_persisted
+      _created_id = subject.id
       expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
+      expect(http_request_tracker.first[:response]).to match_response_schema("shift/v1/documents/member/address")
 
     end
   end
@@ -24,11 +37,15 @@ RSpec.describe "Addresses API end to end spec", vcr: true do
     it "should have no compound documents if not requested using include"
     context "customer account relationship" do
       it "should have links to the customer account that are accessible"
-      it "should have compound documents when requested"
+      it "should have compound documents when requested" do
+        subject = model.includes("customer_account").find(created_id).first
+        expect(subject.id).to eql created_id
+      end
     end
 
   end
   context "#index" do
+
 
   end
   context "#archive" do
