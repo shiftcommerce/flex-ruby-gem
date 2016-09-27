84288d407c141caf50116ce2229208870eeffc43
Gary Taylor <gary.taylor@hismessages.com>
2016-09-27 11:44:33 +0100

    Create action of variant at first stage

--- spec/e2e/addresses_api_e2e_spec.rb
+++ spec/e2e/addresses_api_e2e_spec.rb
@@ -2,14 +2,14 @@ require "e2e_spec_helper"
 RSpec.describe "Addresses API end to end spec", vcr: true do
   include_context "context store"
   let(:model) { FlexCommerce::Address }
-  let(:created_id) { context_store[:created_id] }
+  let(:created_id) { context_store.created_resource.id }
   let(:created_customer_account_id) { context_store[:created_customer_account_id] }
   before(:context) do
     context_store.created_customer_account_id = FlexCommerce::CustomerAccount.create!(email: "testaccount#{Time.now.to_f}@domain.com", reference: "ref_#{Time.now.to_f}", password: "12345test67890").id
     http_request_tracker.clear
   end
   after(:context) do
-    FlexCommerce::Address.includes("").find(context_store.created_id).first.destroy unless context_store.created_id.nil?
+    context_store.created_resource.destroy unless context_store.created_resource.nil?
     FlexCommerce::CustomerAccount.includes("").find(context_store.created_customer_account_id).first.destroy unless context_store.created_customer_account_id.nil?
   end
   context "#create" do
@@ -26,7 +26,7 @@ RSpec.describe "Addresses API end to end spec", vcr: true do
                    country: "UK",
                    customer_account_id: created_customer_account_id
       expect(subject).to be_persisted
-      context_store.created_id = subject.id
+      context_store.created_resource = subject
       expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
       expect(http_request_tracker.first[:response]).to match_response_schema("shift/v1/documents/member/address")
 
--- spec/e2e/variants_api_e2e_spec.rb
+++ spec/e2e/variants_api_e2e_spec.rb
@@ -1,25 +1,35 @@
 require "e2e_spec_helper"
-RSpec.describe "Variants API end to end spec" do
-  it_should_behave_like "crud endpoints" do
-    let(:base_attributes) { attributes_for(:variant) }
-    let(:attributes_to_update) { { title: "#{base_attributes[:title]}CHANGED" } }
-    let(:model) { FlexCommerce::Variant }
+RSpec.describe "Variants API end to end spec", vcr: true do
+  include_context "context store"
+  let(:model) { FlexCommerce::Variant }
+  before(:context) do
+    context_store.uuid = SecureRandom.uuid
+    context_store.foreign_resources = {}
+    context_store.foreign_resources[:product] = FlexCommerce::Product.create! title: "Title for product 1 for variant #{context_store.uuid}",
+                                                                              reference: "reference for product 1 for variant #{context_store.uuid}",
+                                                                              content_type: "markdown"
   end
-  context "related products" do
-
-  end
-  context "related asset files" do
-
-  end
-  context "related markdown prices" do
-
-  end
-  context "bulk import" do
-
-  end
-  context "bulk delete" do
-
+  after(:context) do
+    context_store.created_resource.destroy unless context_store.created_resource.nil? || !context_store.created_resource.persisted?
+    context_store.foreign_resources.values.reverse.each do |resource|
+      resource.destroy if resource.persisted?
+    end
   end
 
+  context "#create" do
+    it "should persist when valid attributes are used" do
+      uid = context_store.uuid
+      context_store.created_resource = subject = model.create title: "Title for Test Variant #{uid}",
+                             description: "Description for Test Variant #{uid}",
+                             reference: "reference_for_test_variant_#{uid}",
+                             price: 5.50,
+                             price_includes_taxes: false,
+                             sku: "sku_for_test_variant_#{uid}",
+                             product_id: context_store.foreign_resources[:product].id
+      expect(subject.errors).to be_empty
+      expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
+      expect(http_request_tracker.first[:response]).to match_response_schema("shift/v1/documents/member/variant")
+    end
+  end
 
 end
