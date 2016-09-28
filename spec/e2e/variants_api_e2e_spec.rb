1d96d4c595ceaa2c29c88390e45a6d90f364d662
Gary Taylor <gary.taylor@hismessages.com>
2016-09-28 11:27:56 +0100

    Added a few comments explaining things

--- spec/e2e/variants_api_e2e_spec.rb
+++ spec/e2e/variants_api_e2e_spec.rb
@@ -1,10 +1,24 @@
 require "e2e_spec_helper"
 RSpec.describe "Variants API end to end spec", vcr: true do
+  # As the "before context" blocks cannot access let vars, the "context store" simply defines a method called "context_store"
+  # that stores whatever you want - it is an "OpenStruct" so you can just write anything to it and read it back at any time
+  # This is cleared at the start of the context, but the idea is so that you can share stuff between examples.
+  # This obviously means that the examples are tied together - in terms of the read, update and delete methods all rely
+  # on having created an object in the first place.
+  # This also means that this test suite must be run in the order defined, not random.
   include_context "context store"
+
+  # We define the model in advance, mainly allowing the code in the examples to be fairly generic and can be copied / pasted
+  # into other tests without changing the model all over the place.
   let(:model) { FlexCommerce::Variant }
+
+  # A few convenience lets just to avoid writing context_store.uuid for example
   let(:uuid) { context_store.uuid }
   let(:created_resource) { context_store.created_resource }
   let(:created_product) { context_store.foreign_resources[:product] }
+
+  # As setting up for testing can be very expensive, we do it only at the start of then context
+  # it is then our responsibility to tidy up at the end of the context.
   before(:context) do
     context_store.uuid = SecureRandom.uuid
     context_store.foreign_resources = OpenStruct.new
@@ -12,6 +26,7 @@ RSpec.describe "Variants API end to end spec", vcr: true do
                                                                               reference: "reference for product 1 for variant #{context_store.uuid}",
                                                                               content_type: "markdown"
   end
+  # Clean up time - delete stuff in the reverse order to give us more chance of success
   after(:context) do
     context_store.created_resource.destroy unless context_store.created_resource.nil? || !context_store.created_resource.persisted?
     context_store.foreign_resources.values.reverse.each do |resource|
@@ -35,7 +50,6 @@ RSpec.describe "Variants API end to end spec", vcr: true do
   end
   context "#read" do
     context "collection" do
-
     end
     context "member" do
       it "should have the correct default relationships included" do
@@ -74,10 +88,7 @@ RSpec.describe "Variants API end to end spec", vcr: true do
                                                                                        reference: "reference_for_asset_file_1_for_variant_#{uuid}",
                                                                                        asset_file: "data:image/png;base64,#{Base64.encode64(File.read(asset_file_fixture_file))}",
                                                                                        asset_folder_id: context_store.foreign_resources.asset_folder.id
-          context_store.foreign_resources.product.update_attributes asset_files_attributes: [ name: "name for Asset file 1 for Test Variant #{uuid}",
-                                                                                      reference: "reference_for_asset_file_1_for_variant_#{uuid}",
-                                                                                      asset_file: "data:image/png;base64,#{Base64.encode64(File.read(asset_file_fixture_file))}",
-                                                                                      asset_folder_id: context_store.foreign_resources.asset_folder.id ]
+          context_store.foreign_resources.product.add_asset_files([context_store.foreign_resources.asset_file])
         end
         it "should exist" do
           subject = model.find(created_resource.id)
@@ -85,12 +96,12 @@ RSpec.describe "Variants API end to end spec", vcr: true do
         end
         it "should be loadable using compound documents" do
           subject = model.includes("asset_files").find(created_resource.id).first
-          expect(subject.asset_files).to contain_exactly an_object_having_attributes created_product.attributes.slice(:id, :title, :reference, :content_type)
+          expect(subject.asset_files).to contain_exactly an_object_having_attributes context_store.foreign_resources.asset_file.attributes.slice(:id, :title, :reference, :content_type)
           expect(http_request_tracker.length).to eql 1
         end
         it "should be loadable using links" do
           subject = model.includes("").find(created_resource.id).first
-          expect(subject.asset_files).to contain_exactly an_object_having_attributes created_product.attributes.slice(:id, :title, :reference, :content_type)
+          expect(subject.asset_files).to contain_exactly an_object_having_attributes context_store.foreign_resources.asset_file.attributes.slice(:id, :title, :reference, :content_type)
           expect(http_request_tracker.length).to eql 2
         end
 
