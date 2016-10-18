19005b6e9224c83317cf06c4215c86d2e5fbb37a
Gary Taylor <gary.taylor@hismessages.com>
2016-10-18 16:45:15 +0100

    Further developed variant spec

--- spec/e2e/variants_api_e2e_spec.rb
+++ spec/e2e/variants_api_e2e_spec.rb
@@ -3,29 +3,45 @@ RSpec.describe "Variants API end to end spec", vcr: true do
   # As the "before context" blocks cannot access let vars, the "context store" simply defines a method called "context_store"
   # that stores whatever you want - it is an "OpenStruct" so you can just write anything to it and read it back at any time
   # This is cleared at the start of the context, but the idea is so that you can share stuff between examples.
-  # This obviously means that the examples are tied together - in terms of the read, update and delete methods all rely
-  # on having created an object in the first place.
-  # This also means that this test suite must be run in the order defined, not random.
+  # This is useful where you have some expensive or complex setups that dont need to be done for each example, but instead the setup is done
+  # for the context.
+  # In this file, we use the context store as a global store, mainly for tracking foreign resources to ensure they get setup and deleted correctly
+  # it can also be used by nested contexts as long as they tidy up after themselves.
   include_context "context store"
 
   # We define the model in advance, mainly allowing the code in the examples to be fairly generic and can be copied / pasted
   # into other tests without changing the model all over the place.
   let(:model) { FlexCommerce::Variant }
 
-  # A few convenience lets just to avoid writing context_store.uuid for example
-  let(:uuid) { context_store.uuid }
-  let(:created_resource) { context_store.created_resource }
-  let(:created_product) { context_store.foreign_resources[:product] }
+  # A few convenience methods just to avoid writing context_store.uuid for example
+  def uuid
+    context_store.uuid
+  end
+
+  def created_product
+    context_store.foreign_resources[:product]
+  end
+
+  def asset_file_fixture_file
+    # Create an asset file for use by the test
+    File.expand_path("../support_e2e/fixtures/asset_file.png", File.dirname(__FILE__))
+  end
 
   # As setting up for testing can be very expensive, we do it only at the start of then context
   # it is then our responsibility to tidy up at the end of the context.
+  # In this case the expensive thing is the product, but the uuid is conveniently setup here to give us a unique id
+  # for the whole context.  Useful for when attributes in your test data must be unique.
   before(:context) do
     context_store.uuid = SecureRandom.uuid
     context_store.foreign_resources = OpenStruct.new
     context_store.foreign_resources[:product] = FlexCommerce::Product.create! title: "Title for product 1 for variant #{context_store.uuid}",
                                                                               reference: "reference for product 1 for variant #{context_store.uuid}",
                                                                               content_type: "markdown"
+    context_store.foreign_resources.asset_folder = FlexCommerce::AssetFolder.create! name: "asset folder for Test Variant #{context_store.uuid}",
+                                                                                     reference: "reference_for_asset_folder_1_for_variant_#{context_store.uuid}"
+
   end
+
   # Clean up time - delete stuff in the reverse order to give us more chance of success
   after(:context) do
     context_store.foreign_resources.values.reverse.each do |resource|
@@ -33,7 +49,13 @@ RSpec.describe "Variants API end to end spec", vcr: true do
     end
   end
 
+  # |------------------------------------------------------------------------|
+  # |                    Creating Variants                                   |
+  # |                                                                        |
+  # |------------------------------------------------------------------------|
   context "#create" do
+    # All create examples will use the same subject but "attributes_for_examples" will vary
+    # This is done to allow the after each block to keep things tidy automatically
     subject! { model.create attributes_for_examples }
     after(:each) { subject.destroy if subject.persisted? }
     context "with invalid attributes" do
@@ -66,6 +88,7 @@ RSpec.describe "Variants API end to end spec", vcr: true do
         expect(http_request_tracker.first[:response]).to match_response_schema("shift/v1/documents/member/variant")
       end
     end
+
     context "valid attributes with valid markdown prices to be created" do
       let(:attributes_for_examples) do
         {
@@ -80,19 +103,51 @@ RSpec.describe "Variants API end to end spec", vcr: true do
             markdown_prices_resources: [FlexCommerce::MarkdownPrice.new(price: 1.10, start_at: 2.days.ago, end_at: 10.days.since)]
         }
       end
-      it "should be able to add a new markdown price" do
+      it "should create the resource with a markdown price" do
         resource = model.includes("markdown_prices").find(subject.id).first
         expect(resource.markdown_prices).to include(an_object_having_attributes price: 1.10)
       end
     end
+
+    context "valid attributes with valid asset files to be created" do
+      let(:asset_file) do
+        FlexCommerce::AssetFile.new(name: "name for Asset file 1 for Test Variant #{uuid}",
+                      reference: "reference_for_asset_file_1_for_variant_#{uuid}",
+                      asset_file: "data:image/png;base64,#{Base64.encode64(File.read(asset_file_fixture_file))}",
+                      asset_folder_id: context_store.foreign_resources.asset_folder.id)
+      end
+      let(:attributes_for_examples) do
+        {
+            title: "Title for Test Variant #{uuid} temp 1",
+            description: "Description for Test Variant #{uuid}",
+            reference: "reference_for_test_variant_#{uuid}_temp_1",
+            price: 5.50,
+            price_includes_taxes: false,
+            sku: "sku_for_test_variant_#{uuid}_temp_1",
+            product_id: created_product.id,
+
+            asset_files_resources: [asset_file]
+        }
+      end
+      it "should create the resource with an asset file" do
+        aggregate_failures "validating resource has the asset file added" do
+          resource = model.includes("asset_files").find(subject.id).first
+          expect(resource.asset_files).to include(an_object_having_attributes reference: asset_file.reference)
+          resource.asset_files.first.destroy if resource.asset_files.first.present?
+        end
+      end
+    end
   end
+
+  # |------------------------------------------------------------------------|
+  # |                    Reading Variants                                    |
+  # |                                                                        |
+  # |------------------------------------------------------------------------|
   context "#read" do
     context "collection" do
     end
     context "member" do
       before(:context) do
-        created_product = context_store.foreign_resources[:product]
-        uuid = context_store.uuid
         context_store.created_resource = FlexCommerce::Variant.create title: "Title for Test Variant #{uuid}",
                                                        description: "Description for Test Variant #{uuid}",
                                                        reference: "reference_for_test_variant_#{uuid}",
@@ -107,6 +162,26 @@ RSpec.describe "Variants API end to end spec", vcr: true do
         context_store.delete_field(:created_resource)
       end
 
+      context "with caching", caching: true do
+        let!(:created_resource) do
+          uuid = SecureRandom.uuid
+          FlexCommerce::Variant.create title: "Title for Test Variant #{uuid}",
+                                       description: "Description for Test Variant #{uuid}",
+                                       reference: "reference_for_test_variant_#{uuid}",
+                                       price: 5.50,
+                                       price_includes_taxes: false,
+                                       sku: "sku_for_test_variant_#{uuid}",
+                                       product_id: created_product.id
+        end
+        it "should indicate the first request after creation is not cached and the second request is" do
+          http_request_tracker.clear
+          model.find(created_resource.id)
+          model.find(created_resource.id)
+          # @TODO Work out what these headers should contain when going via fastly
+          expect(http_request_tracker.first[:response].headers.keys).to include 'Surrogate-Key'
+          expect(http_request_tracker[1][:response].headers.keys).to include 'Surrogate-Key'
+        end
+      end
       it "should have the correct default relationships included" do
         subject = model.find(context_store.created_resource.id)
         http_request_tracker.clear
@@ -118,18 +193,18 @@ RSpec.describe "Variants API end to end spec", vcr: true do
 
       context "product relationship" do
         it "should exist" do
-          subject = model.find(created_resource.id)
+          subject = model.find(context_store.created_resource.id)
           expect(subject.relationships.product).to be_present
         end
         it "should be loadable using compound documents" do
-          subject = model.includes("product").find(created_resource.id).first
+          subject = model.includes("product").find(context_store.created_resource.id).first
           expect(subject.product).to have_attributes created_product.attributes.slice(:id, :title, :reference, :content_type)
           expect(http_request_tracker.length).to eql 1
           expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
           expect(http_request_tracker.first[:response]).to match_response_schema("shift/v1/documents/member/variant")
         end
         it "should be loadable using links" do
-          subject = model.includes("").find(created_resource.id).first
+          subject = model.includes("").find(context_store.created_resource.id).first
           expect(subject.product).to have_attributes created_product.attributes.slice(:id, :title, :reference, :content_type)
           expect(http_request_tracker.length).to eql 2
           expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
@@ -139,11 +214,7 @@ RSpec.describe "Variants API end to end spec", vcr: true do
 
       context "asset_files relationship" do
         before(:context) do
-          # Create an asset file for use by the test
-          asset_file_fixture_file = File.expand_path("../support_e2e/fixtures/asset_file.png", File.dirname(__FILE__))
           uuid = context_store.uuid
-          context_store.foreign_resources.asset_folder = FlexCommerce::AssetFolder.create! name: "asset folder for Test Variant #{uuid}",
-                                                                                           reference: "reference_for_asset_folder_1_for_variant_#{uuid}"
           context_store.foreign_resources.asset_file = FlexCommerce::AssetFile.create! name: "name for Asset file 1 for Test Variant #{uuid}",
                                                                                        reference: "reference_for_asset_file_1_for_variant_#{uuid}",
                                                                                        asset_file: "data:image/png;base64,#{Base64.encode64(File.read(asset_file_fixture_file))}",
@@ -151,18 +222,18 @@ RSpec.describe "Variants API end to end spec", vcr: true do
           context_store.foreign_resources.product.add_asset_files([context_store.foreign_resources.asset_file])
         end
         it "should exist" do
-          subject = model.find(created_resource.id)
+          subject = model.find(context_store.created_resource.id)
           expect(subject.relationships.asset_files).to be_present
         end
         it "should be loadable using compound documents" do
-          subject = model.includes("asset_files").find(created_resource.id).first
+          subject = model.includes("asset_files").find(context_store.created_resource.id).first
           expect(subject.asset_files).to contain_exactly an_object_having_attributes context_store.foreign_resources.asset_file.attributes.slice(:id, :title, :reference, :content_type)
           expect(http_request_tracker.length).to eql 1
           expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
           expect(http_request_tracker.first[:response]).to match_response_schema("shift/v1/documents/member/variant")
         end
         it "should be loadable using links" do
-          subject = model.includes("").find(created_resource.id).first
+          subject = model.includes("").find(context_store.created_resource.id).first
           expect(subject.asset_files).to contain_exactly an_object_having_attributes context_store.foreign_resources.asset_file.attributes.slice(:id, :title, :reference, :content_type)
           expect(http_request_tracker.length).to eql 2
           expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
@@ -182,18 +253,18 @@ RSpec.describe "Variants API end to end spec", vcr: true do
           context_store.foreign_resources.markdown_price.destroy if context_store.foreign_resources.markdown_price.persisted?
         end
         it "should exist" do
-          subject = model.find(created_resource.id)
+          subject = model.find(context_store.created_resource.id)
           expect(subject.relationships.markdown_prices).to be_present
         end
         it "should be loadable using compound documents" do
-          subject = model.includes("markdown_prices").find(created_resource.id).first
+          subject = model.includes("markdown_prices").find(context_store.created_resource.id).first
           expect(subject.markdown_prices).to contain_exactly an_object_having_attributes context_store.foreign_resources.markdown_price.attributes.slice(:id, :price, :start_at, :end_at)
           expect(http_request_tracker.length).to eql 1
           expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
           expect(http_request_tracker.first[:response]).to match_response_schema("shift/v1/documents/member/variant")
         end
         it "should be loadable using links" do
-          subject = model.includes("").find(created_resource.id).first
+          subject = model.includes("").find(context_store.created_resource.id).first
           expect(subject.markdown_prices).to contain_exactly an_object_having_attributes context_store.foreign_resources.markdown_price.attributes.slice(:id, :price, :start_at, :end_at)
           expect(http_request_tracker.length).to eql 2
           expect(http_request_tracker.first[:response]).to match_response_schema("jsonapi/schema")
@@ -203,6 +274,11 @@ RSpec.describe "Variants API end to end spec", vcr: true do
       end
     end
   end
+
+  # |------------------------------------------------------------------------|
+  # |                    Updating Variants                                   |
+  # |                                                                        |
+  # |------------------------------------------------------------------------|
 
   context "#update" do
     let!(:created_resource) do
@@ -258,7 +334,7 @@ RSpec.describe "Variants API end to end spec", vcr: true do
 
     context "markdown_prices relationship" do
       it "should be able to add a new markdown price" do
-        result = subject.update_attributes(title: "Something really different", markdown_prices_resources: [FlexCommerce::MarkdownPrice.new(price: 1.10, start_at: 2.days.ago, end_at: 10.days.since)])
+        result = subject.update_attributes(markdown_prices_resources: [FlexCommerce::MarkdownPrice.new(price: 1.10, start_at: 2.days.ago, end_at: 10.days.since)])
         expect(result).to be_truthy
         resource = model.includes("markdown_prices").find(created_resource.id).first
         expect(resource.markdown_prices).to include(an_object_having_attributes price: 1.10)
@@ -277,6 +353,10 @@ RSpec.describe "Variants API end to end spec", vcr: true do
     end
   end
 
+  # |------------------------------------------------------------------------|
+  # |                    Deleting Variants                                   |
+  # |                                                                        |
+  # |------------------------------------------------------------------------|
   context "#delete" do
     it "should delete"
   end
