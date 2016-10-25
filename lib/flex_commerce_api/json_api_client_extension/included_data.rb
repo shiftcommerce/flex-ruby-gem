e9be1558d4a6578ca660deb97bd9789c5ed6cfd0
Keith McDonnell <keith@dancingtext.com>
2016-10-25 15:52:19 +0100

    Fix loading paths (#84)
    * Fix loading paths

--- lib/flex_commerce_api/json_api_client_extension/included_data.rb
+++ lib/flex_commerce_api/json_api_client_extension/included_data.rb
@@ -1,12 +1,26 @@
-module FlexCommerce
+require 'json_api_client/included_data'
+
+module FlexCommerceApi
   module JsonApiClientExtension
     class IncludedData < ::JsonApiClient::IncludedData
+      def initialize(result_set, *args)
+        super(result_set, *args)
+        @result_set = result_set
+      end
+
       private
 
-      # should return a resource record of some type for this linked document
+      # attempt to load the record from included data first, failing that, look
+      # in the root resource(s) for the record
       def record_for(link_def)
-        return nil unless data.key?(link_def["type"])
-        data[link_def["type"]][link_def["id"]]
+        data.dig(link_def["type"], link_def["id"]) || root_record_for(link_def)
+      end
+
+      def root_record_for(link_def)
+        @result_set.find do |resource|
+          resource.attributes["type"] == link_def["type"] &&
+          resource.attributes["id"] == link_def["id"]
+        end
       end
     end
   end
