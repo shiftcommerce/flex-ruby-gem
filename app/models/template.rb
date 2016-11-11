1692d63a8948615c631577e2b0576c4ec4b0f4d5
Josh Tatterton <tattsy@hotmail.co.uk>
2016-11-11 14:47:01 +0000

    Remove my method missing section finder

--- app/models/template.rb
+++ app/models/template.rb
@@ -5,17 +5,5 @@ module FlexCommerce
   #
   class Template < FlexCommerceApi::ApiBase
     has_many :sections, class_name: "::FlexCommerce::Section"
-
-    def method_missing(method, *args)
-      if relationships and relationships.has_attribute?(method)
-        super
-      elsif has_attribute?(method) || method.to_s=~(/=$/) || method.to_s=~/!$/
-        super
-      elsif sections.map { |s| s.reference.to_sym }.include?(method)
-        sections.select { |s| s.reference.to_sym == method }.first
-      else
-        nil
-      end
-    end
   end
 end
