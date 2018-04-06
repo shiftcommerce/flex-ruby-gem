b15e45178aaad20fb123074832ce96d8822f33f3
praweb <prasanna.virigineni@gmail.com>
2018-04-06 13:58:10 +0100

    Refactor code

--- app/services/surrogate_keys.rb
+++ app/services/surrogate_keys.rb
@@ -1,16 +1,26 @@
+# Class: SurrogateKeys
+# This class implements the getter and setter methods for surrogate keys
+# This is used in 'CartManagement' in rails-site
+# 
 module FlexCommerce
   class SurrogateKeys
 
-    def self.get_keys(key: )
-      surrogate_keys = Thread.current[:shift_surrogate_keys]
+    def self.get_keys(key:)
+      # Return all the surrogate keys, if not requested for a particular key
       return surrogate_keys.join(' ') unless key.nil?
-      surrogate_keys = surrogate_keys.nil?  ? "" : surrogate_keys.select { |k| k.include?(key) }.join(' ')
+      # Select only the surrogate keys, which includes the requested key
+      surrogate_keys.nil?  ? "" : surrogate_keys.select { |k| k.include?(key) }.join(' ')
     end
 
     def self.set_keys(keys)
-      if Thread.current[:shift_surrogate_keys]
+      # Check if surrogate key is available, then update its value
+      if surrogate_keys
         Thread.current[:shift_surrogate_keys] += keys
       end
+    end
+
+    def self.surrogate_keys
+      Thread.current[:shift_surrogate_keys]
     end
 
   end
