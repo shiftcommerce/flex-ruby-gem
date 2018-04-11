76c5f1f222e4cd37c2f64b403d13872563648b5d
praweb <prasanna.virigineni@gmail.com>
2018-04-11 09:17:19 +0100

    Refactor surrogate keys methods

--- app/services/surrogate_keys.rb
+++ app/services/surrogate_keys.rb
@@ -5,23 +5,18 @@
 module FlexCommerce
   class SurrogateKeys
 
-    def self.keys(filter:)
-      # Return Empty string, if surrogate keys are not available on thread
-      return "" if surrogate_keys.nil?
+    def self.keys(filter: [])
+      surrogate_keys = Thread.current[:shift_surrogate_keys]
       # Return all the surrogate keys, if not requested for a particular key
-      return surrogate_keys.join(' ') unless filter.nil?
+      return surrogate_keys unless filter.nil?
       # Select only the surrogate keys, which includes the requested key
-      surrogate_keys.nil?  ? "" : surrogate_keys.select { |k| k.include?(filter) }.join(' ')
+      surrogate_keys.nil?  ? [] : surrogate_keys.select { |k| k.include?(filter) }
     end
 
     def self.append_keys(*keys)
       # Check if surrogate key is available, then update its value
-      return [] if surrogate_keys.nil?
+      Thread.current[:shift_surrogate_keys] ||= []
       Thread.current[:shift_surrogate_keys] += keys.flatten
-    end
-
-    def self.surrogate_keys
-      Thread.current[:shift_surrogate_keys]
     end
 
   end
