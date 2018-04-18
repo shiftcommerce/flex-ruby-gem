6594376d1fb9d5518a66fca43ddc20be9a2cd2ec
Mufudzi Masaire <mufudzimasaire@users.noreply.github.com>
2018-04-18 10:09:13 +0100

    Issue: Add TaxCode feature (#128)

---
+++ spec/factories/tax_codes.rb
@@ -0,0 +1,27 @@
+require "flex_commerce_api"
+
+FactoryBot.define do  
+  factory :tax_code, class: ::FlexCommerce::TaxCode do
+    sequence(:code) { |n| "Code#{n}suffix" }
+
+    starts_at   { Time.current }
+    ends_at     { 1.days.from_now }
+    country     "GB"
+    rate        0.1
+  end
+
+  factory :tax_codes_from_fixture, class: JsonStruct do
+    obj = JsonStruct.new(JSON.parse(File.read("spec/fixtures/tax_codes/multiple.json")))
+    obj.each_pair do |key, value|
+      send(key, value)
+    end
+  end
+
+  factory :tax_code_from_fixture, class: JsonStruct do
+    obj = JsonStruct.new(JSON.parse(File.read("spec/fixtures/tax_codes/single.json")))
+    obj.each_pair do |key, value|
+      send(key, value)
+    end
+  end
+  
+end
