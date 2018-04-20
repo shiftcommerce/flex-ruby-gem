d086a32aaf0979cd51832901ca3959b181a060fa
Richard Watkins <richardwatkins1@gmail.com>
2018-04-20 13:33:22 +0100

    spec updated

--- spec/integration/taxonomy_spec.rb
+++ spec/integration/taxonomy_spec.rb
@@ -15,10 +15,8 @@ RSpec.describe FlexCommerce::Taxonomy do
         stub_request(:get, "#{api_root}/taxonomies/1.json_api").
           with(headers: { "Accept" => "application/vnd.api+json" }).
           to_return(body: build(:taxonomy_from_fixture).to_json, status: response_status, headers: default_headers)
-
         # Act
         record = described_class.find(1)
-
         # Assert
         aggregate_failures do
           expect(record).to be_present
@@ -45,11 +43,9 @@ RSpec.describe FlexCommerce::Taxonomy do
       it "should destroy the record" do
         # Arrange
         resource = build(:taxonomy, id: 1)
-
         stub_request(:delete, "#{api_root}/taxonomies/1.json_api").
           with(headers: { "Accept" => "application/vnd.api+json" }).
           to_return(status: 204, headers: default_headers)
-
         # Assert
         expect(resource.destroy).to eq(true)
       end
@@ -59,11 +55,9 @@ RSpec.describe FlexCommerce::Taxonomy do
       it "should raise an AccessDenied exception" do
         # Arrange
         resource = build(:taxonomy, id: 1)
-
         stub_request(:delete, "#{api_root}/taxonomies/1.json_api").
           with(headers: { "Accept" => "application/vnd.api+json" }).
           to_return(body: '', status: 403, headers: nil)
-
         # Act & Assert
         expect { resource.destroy }.to raise_exception(::FlexCommerceApi::Error::AccessDenied)
       end
