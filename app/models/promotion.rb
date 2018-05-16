14eac10696e697fa19054bfaa439d5985b936ff3
praweb <prasanna.virigineni@gmail.com>
2018-05-16 15:26:07 +0100

    add comments

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -6,7 +6,14 @@ module FlexCommerce
   # This model provides access to the flex commerce promotion
   #
   class Promotion < FlexCommerceApi::ApiBase
+    # This adds the ability to call archive method on Promotion
+    # 
+    # @usage FlexCommerce::Promotion.find(<id>).archive
     custom_endpoint :archive, on: :member, request_method: :patch
+
+    # This adds the ability to call unarchive method on Promotion
+    # 
+    # @usage FlexCommerce::Promotion.find(<id>).unarchive
     custom_endpoint :unarchive, on: :member, request_method: :patch
   end
 end
