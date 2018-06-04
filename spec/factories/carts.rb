90fd20fffc9fbb8e8668093556c7707180f1cfb6
praweb <prasanna.virigineni@gmail.com>
2018-06-04 14:28:35 +0100

    Cart and line items factories not used in payment specs

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -1,21 +1,6 @@
-require "flex_commerce_api"
-
 FactoryBot.define do
   factory :cart, class: JsonStruct do
-    transient do
-      line_items_count 3  # number of line items to create in traits
-      line_item_unit_quantity 1
-    end
 
-    trait :with_line_items do
-      after(:create) do |cart, evaluator|
-        evaluator.line_items_count.times do
-          variant = create(:variant)
-          create(:line_item, :cart_variant, container_id: cart.id,
-                 item_id: variant.id, item_type: variant.class.to_s, unit_quantity: evaluator.line_item_unit_quantity)
-        end
-      end
-    end
   end
 
   factory :cart_from_fixture, class: JsonStruct do
--- spec/factories/line_items.rb
+++ spec/factories/line_items.rb
@@ -1,16 +1,8 @@
-require "flex_commerce_api"
-
 FactoryBot.define do
   factory :line_item_from_fixture, class: JsonStruct do
     obj = JsonStruct.new(JSON.parse(File.read("spec/fixtures/line_items/singular.json")))
     obj.each_pair do |key, value|
       send(key, value)
     end
-  end
-
-  factory :api_line_item, class: ::FlexCommerce::LineItem do
-    unit_quantity 1
-
-    after(:create) {|line_item| line_item.container.line_items.reset }
   end
 end
