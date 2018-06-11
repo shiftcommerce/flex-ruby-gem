00702c50b9d99dccc9a07e966c8df499e8979845
Richard Watkins <richardwatkins1@gmail.com>
2018-06-11 11:29:27 +0100

    updated association

--- app/models/template_section.rb
+++ app/models/template_section.rb
@@ -7,7 +7,7 @@ module FlexCommerce
   #
   #
   class TemplateSection < FlexCommerceApi::ApiBase
-    has_one :template_definition, class_name: "::FlexCommerce::TemplateDefinition"
+    belongs_to :template_definition, class_name: "::FlexCommerce::TemplateDefinition"
     has_many :template_components, class_name: "::FlexCommerce::TemplateComponent"
   end
 end
