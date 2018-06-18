10855154e3a8ab99381369774ec69ac966f40d0a
praweb <prasanna.virigineni@gmail.com>
2018-06-18 17:04:43 +0100

    Modify PaymentProviderSetup from class to module

--- app/models/payment_provider_setup.rb
+++ app/models/payment_provider_setup.rb
@@ -3,7 +3,7 @@ require "flex_commerce_api/api_base"
 # @module FlexCommerce
 module FlexCommerce
   #
-  # @class PaymentProviderSetup
+  # @module PaymentProviderSetup
   # A flex commerce PaymentProvider model
   #
   # This model is used to define a payment gateway and provide instructions
@@ -11,22 +11,21 @@ module FlexCommerce
   # (i.e. redirect to paypal etc..)
   #
   #
-  class PaymentProviderSetup
-    include ActiveModel::Model
-    attr_accessor :setup_type, :redirect, :redirect_url, :cart, :callback_url, :remote_ip, :setup
-    attr_accessor :ip_address, :success_url, :cancel_url, :allow_shipping_change, :use_mobile_payments, :payment_provider_id
+  module PaymentProviderSetup
 
-    #  @method call
+    class << self; attr_accessor :setup_type, :redirect_url; end
+    
+    # @method call
     # 
     # Triggers call to PaypalExpress::Setup service class to setup paypal
     # 
     # @return [FlexCommerce::PaymentAdditionalInfo]
-    def call
+    def self.call(cart:, payment_provider_id:, ip_address:, success_url:, cancel_url:, allow_shipping_change:, callback_url:, use_mobile_payments:)
       # Call PaypalExpress::Setup service class, which will talk to active merchant gem for setup process
       setup ||= ::FlexCommerce::Payments::PaypalExpress::Setup.new(cart: cart,
         payment_provider_setup: self,
-        payment_provider: payment_provider,
-        ip_address: remote_ip,
+        payment_provider: self.payment_provider(payment_provider_id),
+        ip_address: ip_address,
         success_url: success_url,
         cancel_url: cancel_url,
         allow_shipping_change: allow_shipping_change,
@@ -36,8 +35,8 @@ module FlexCommerce
 
     private
 
-    def payment_provider
-      FlexCommerce::PaymentProvider.all.select{ |p| p.reference == payment_provider_id }.first
+    def self.payment_provider(payment_provider_id)
+      FlexCommerce::PaymentProvider.where(reference: payment_provider_id).first
     end
 
   end
