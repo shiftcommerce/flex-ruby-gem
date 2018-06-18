aa64451b6ff0c480bcdedd009be4257d7486488b
Richard Watkins <richardwatkins1@gmail.com>
2018-06-18 10:27:27 +0100

    markdown association updated, specs added

--- spec/factories/markdown_prices.rb
+++ spec/factories/markdown_prices.rb
@@ -7,4 +7,17 @@ FactoryBot.define do
     end_at    { Faker::Date.between(2.days.ago, Date.today) }
   end
 
+  factory :markdown_prices_from_fixture, class: JsonStruct do
+    obj = JsonStruct.new(JSON.parse(File.read("spec/fixtures/markdown_prices/multiple.json")))
+    obj.each_pair do |key, value|
+      send(key, value)
+    end
+  end
+  factory :markdown_price_from_fixture, class: JsonStruct do
+    obj = JsonStruct.new(JSON.parse(File.read("spec/fixtures/markdown_prices/singular.json")))
+    obj.each_pair do |key, value|
+      send(key, value)
+    end
+  end
+
 end
