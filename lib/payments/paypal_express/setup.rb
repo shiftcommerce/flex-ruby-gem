9d80979c90746dd4ab7d1c521c99be5db17d21ff
praweb <prasanna.virigineni@gmail.com>
2018-07-03 22:37:21 +0100

    Refactoring of Setup Service in progress

--- lib/payments/paypal_express/setup.rb
+++ lib/payments/paypal_express/setup.rb
@@ -9,7 +9,7 @@ module FlexCommerce
       # This is the main class, which talks to ActiveMerchant gem to initiate a transaction using Paypal
       class Setup
         include ::FlexCommerce::Payments::PaypalExpress::Api
-        DEFAULT_DESCRIPTION = "Shift Commerce Order".freeze
+        
 
         # @initialize
         # 
@@ -44,6 +44,7 @@ module FlexCommerce
         def call
           return false unless valid_shipping_method?
           
+
           response = gateway.setup_order(convert_amount(cart.total), paypal_params)
           # If paypal setup went fine, redirect to the paypal page
           if response.success?
@@ -62,58 +63,17 @@ module FlexCommerce
         attr_accessor :description, :cart, :gateway_class, :success_url, :cancel_url, :ip_address, :allow_shipping_change, :callback_url, :shipping_method_model, :use_mobile_payments
 
         def paypal_params
-          base_paypal_params
-            .merge(paypal_shipping_address_params)
-            .merge(paypal_items)
-            .merge(ui_callback_params)
-            .merge(shipping_options_params)
-        end
-
-        def paypal_shipping_address_params
-          return {} unless cart.shipping_address
-          shipping_address = cart.shipping_address
-          {
-            address_override: true,
-            shipping_address: {
-              name: "#{shipping_address.first_name} #{shipping_address.middle_names} #{shipping_address.last_name}",
-              address1: shipping_address.address_line_1,
-              address2: "#{shipping_address.address_line_2} #{shipping_address.address_line_3}",
-              city: shipping_address.city,
-              state: shipping_address.state,
-              country: shipping_address.country,
-              zip: shipping_address.postcode
-
-            }
-          }
-        end
-
-        def base_paypal_params
-          {
-            currency: "GBP",
-            description: description || DEFAULT_DESCRIPTION,
-            ip: ip_address,
-            return_url: success_url,
-            cancel_return_url: cancel_url,
-            subtotal: summary[:subtotal], # As the cart total wont include any shipping if it has no shipping method
-            handling: summary[:handling],
-            tax: 0,
-            shipping: summary[:shipping]
-          }
-        end
-
-        def paypal_items
-          items = summary[:items]
-          {items: items}
-        end
-
-        def ui_callback_params
-          return {} unless allow_shipping_change && shipping_methods.count > 0
-          { 
+          PaypalParams.new(
+            cart: cart,
+            success_url: success_url,
+            cancel_url: cancel_url,
+            ip_address: ip_address,
+            allow_shipping_change: allow_shipping_change,
             callback_url: callback_url,
-            callback_timeout: 6,
-            callback_version: 95,
-            max_amount: convert_amount((cart.total * 1.2) + shipping_methods.last.total + shipping_methods.last.tax) 
-          }
+            shipping_method_model: shipping_method_model,
+            use_mobile_payments: use_mobile_payments,
+            description: description
+          ).call
         end
 
         # @method shipping_methods
@@ -121,22 +81,6 @@ module FlexCommerce
         # @returns shipping methods with promotions applied
         def shipping_methods
           @shipping_methods ||= ShippingMethodsForCart.new(cart: cart, shipping_methods: shipping_method_model.all).call.sort_by(&:total)
-        end
-
-        def shipping_method
-          cart.shipping_method || shipping_methods.first
-        end
-
-        def shipping_options_params
-          return { shipping_options: [] } if !allow_shipping_change || shipping_method.nil?
-          shipping_method_id = shipping_method.id
-          {
-              shipping_options: shipping_methods.map {|sm| {name: sm.label, amount: convert_amount(sm.total), default: sm.id == shipping_method_id, label: "#{sm.description}#{sm.id}"}}
-          }
-        end
-
-        def summary
-          @summary ||= GenerateSummary.new(cart: cart).call
         end
 
         def valid_shipping_method?
