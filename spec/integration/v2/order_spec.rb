007586f994b933817e774ce1fd7342363d91e257
Hubert Pompecki <hpompecki@gmail.com>
2018-07-12 11:41:29 +0100

    Add blank lines for readability

--- spec/integration/v2/order_spec.rb
+++ spec/integration/v2/order_spec.rb
@@ -16,12 +16,16 @@ RSpec.describe FlexCommerce::V2::Order do
   #
   context "using a single resource" do
     let(:singular_resource) { build(:v2_order_from_fixture) }
+
     context "fetching a single order" do
       before :each do
         stub_request(:get, "#{api_root}/orders/1.json_api").with(headers: {"Accept" => "application/vnd.api+json"}).to_return body: singular_resource.to_json, status: response_status, headers: default_headers
       end
+
       subject { subject_class.find(1) }
+      
       it_should_behave_like "a singular resource with an error response"
+
       it "should return the correct top level object" do
         expect(subject).to be_a(subject_class)
       end
