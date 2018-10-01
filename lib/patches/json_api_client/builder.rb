eb1894ce078df836e7ecd520ce4a0e36bbf19fe3
Adam Dullenty <adam.dullenty@shiftcommerce.com>
2018-10-01 09:40:29 +0100

    Add fix for 'last' link

--- lib/patches/json_api_client/builder.rb
+++ lib/patches/json_api_client/builder.rb
@@ -1,9 +1,17 @@
 module JsonApiClient
   module Query
     class Builder
-      # Hack which gets around the problem - obviously not the best
-      # way to fix this!
-      remove_method :last
+      # This is a workaround because in the json_api_client gem they now parse
+      # the "last" link instead of the original behaviour:
+      # https://github.com/JsonApiClient/json_api_client/blob/v1.5.3/lib/json_api_client/query/builder.rb#L69
+      # This causes an error when Faraday parses our params due to the filter
+      # param appearing to be malformed.
+      #
+      # TODO: Look at fixing our links so that we don't see errors when
+      # Faraday parses the params.
+      def last
+        self.to_a.last
+      end
     end
   end
 end
