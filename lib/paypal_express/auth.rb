03b7306ef2672f4f63859811bad645b21bf0efae
praweb <prasanna.virigineni@gmail.com>
2018-11-29 16:45:33 +0000

    Add Paypal Auth Process specs

--- lib/paypal_express/auth.rb
+++ lib/paypal_express/auth.rb
@@ -8,6 +8,7 @@ module FlexCommerce
     #  
     # This service authorises the payment via the Paypal gateway
     class Auth
+      include ::FlexCommerce::PaypalExpress::Api
       
       DEFAULT_CURRENCY = "GBP".freeze
 
@@ -15,66 +16,51 @@ module FlexCommerce
       #
       # @param {String} token - Paypal token
       # @param {String} payer_id - Paypal user id
-      def initialize(total: ,token: , payer_id:, gateway_class: ::ActiveMerchant::Billing::PaypalExpressGateway,)
-        self.total = total
+      def initialize(cart:, token:, payer_id:, gateway_class: ::ActiveMerchant::Billing::PaypalExpressGateway)
+        self.cart = cart
         self.token = token
         self.payer_id = payer_id
+        self.gateway_class = gateway_class
       end
 
       def call
         process_with_gateway
       end
-  
-      # Attempts to authorise the transaction
-      # @raise Payments::Exception::NotAuthorised If the payment was not authorised for any reason
-      delegate :call, to: :gateway_service
-  
+ 
       private
   
-      attr_reader :total, :token, :payer_id
+      attr_accessor :cart, :token, :payer_id, :gateway_class
 
       def process_with_gateway
         # Fetch Order details from Paypal
-        response = gateway.order(convert_amount(total), token: token, payer_id: payer_id, currenct: DEFAULT_CURRENCY)
-        unless paypal_response.success?
+        response = gateway.order(convert_amount(cart.total), token: token, payer_id: payer_id, currency: DEFAULT_CURRENCY)
+        unless response.success?
           unless is_user_error?(response)
             # TODO: Integrate new relic here
             # TODO: Integrate with I18n here
             raise ::FlexCommerce::PaypalExpress::Exception::NotAuthorized.new("Payment not authorised - #{response.message}", response: response)
           end
-          mark_transaction_with_errors!(response)
-          return false
+          return mark_transaction_with_errors!(response)
         end
 
         # Authorizing transaction
-        auth_response = gateway.authorize_transaction(response.params["transaction_id"], convert_amount(total), transaction_entity: "Order", currency: DEFAULT_CURRENCY, payer_id: payer_id)
+        auth_response = gateway.authorize_transaction(response.params["transaction_id"], convert_amount(cart.total), transaction_entity: "Order", currency: DEFAULT_CURRENCY, payer_id: payer_id)
         unless auth_response.success?
           unless is_user_error?(auth_response)
             # TODO Integrate with new relic here
             # TODO Integrate with I18n here
-            raise ::FlexCommerce::PaypalExpress::Exception::NotAuthorised.new("Failed authorising transaction - #{auth_response.message}", response: auth_response)
+            raise ::FlexCommerce::PaypalExpress::Exception::NotAuthorized.new("Failed authorising transaction - #{auth_response.message}", response: auth_response)
           end
-          mark_transaction_with_errors!(response)
-          return false
+          return mark_transaction_with_errors!(auth_response)
         end
 
-        { transaction_id: response.params["transaction_id"], authorization_id: auth_response.params["transaction_id"]}, status: "success", tender_type: "paypal_express" }
+        { transaction_id: response.params["transaction_id"], authorization_id: auth_response.params["transaction_id"]}
       
       # TODO: There should be a time limit on this
       # TODO: And a retry process around this.
       rescue ::ActiveMerchant::ConnectionError => ex
         # TODO: I18n integration for the message
-        raise ::Payments::Exception::ConnectionError.new("Failed authorising transaction due to a connection error.  Original message was #{ex.message}")
-      end
-
-      # Cleanup Later
-
-      def payment_provider
-        @payment_provider ||= PaymentProvider.find_by(reference: transaction.payment_gateway_reference)
-      end
-  
-      def gateway_service
-        @gateway_service ||= "Payments::#{payment_provider.service.camelize}::Auth".constantize.new(payment_provider: payment_provider, payment_transaction: transaction)
+        raise ::FlexCommerce::PaypalExpress::Exception::ConnectionError.new("Failed authorising transaction due to a connection error.  Original message was #{ex.message}")
       end
     end
   end
