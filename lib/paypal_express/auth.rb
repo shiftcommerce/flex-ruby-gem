7a6b00ecbc3ccc4a19bf3f0a63fd3365cd9a45a0
praweb <prasanna.virigineni@gmail.com>
2018-12-05 11:21:17 +0000

    Add Retry module, and add retries to the paypal api calls

--- lib/paypal_express/auth.rb
+++ lib/paypal_express/auth.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative 'api'
+require 'retry'
 
 # @module FlexCommerce::PaypalExpress
 module FlexCommerce
@@ -8,18 +9,21 @@ module FlexCommerce
     #  
     # This service authorises the payment via the Paypal gateway
     class Auth
+      include ::Retry
       include ::FlexCommerce::PaypalExpress::Api
       
-      DEFAULT_CURRENCY = "GBP".freeze
+      DEFAULT_CURRENCY = "GBP"
+      MAX_NO_OF_RETRIES = 2
 
       # @initialize
       #
       # @param {String} token - Paypal token
       # @param {String} payer_id - Paypal user id
-      def initialize(cart:, token:, payer_id:, gateway_class: ::ActiveMerchant::Billing::PaypalExpressGateway)
+      def initialize(cart:, token:, payer_id:, payment_transaction:, gateway_class: ::ActiveMerchant::Billing::PaypalExpressGateway)
         self.cart = cart
         self.token = token
         self.payer_id = payer_id
+        self.payment_transaction = payment_transaction
         self.gateway_class = gateway_class
       end
 
@@ -29,11 +33,11 @@ module FlexCommerce
  
       private
   
-      attr_accessor :cart, :token, :payer_id, :gateway_class
+      attr_accessor :cart, :token, :payer_id, :payment_transaction, :gateway_class
 
       def process_with_gateway
         # Fetch Order details from Paypal
-        response = gateway.order(convert_amount(cart.total), token: token, payer_id: payer_id, currency: DEFAULT_CURRENCY)
+        response = do_express_checkout_payment
         unless response.success?
           unless is_user_error?(response)
             # TODO: Integrate new relic here
@@ -44,7 +48,7 @@ module FlexCommerce
         end
 
         # Authorizing transaction
-        auth_response = gateway.authorize_transaction(response.params["transaction_id"], convert_amount(cart.total), transaction_entity: "Order", currency: DEFAULT_CURRENCY, payer_id: payer_id)
+        auth_response = do_authorization
         unless auth_response.success?
           unless is_user_error?(auth_response)
             # TODO Integrate with new relic here
@@ -54,13 +58,27 @@ module FlexCommerce
           return mark_transaction_with_errors!(auth_response)
         end
 
-        { transaction_id: response.params["transaction_id"], authorization_id: auth_response.params["transaction_id"]}
-      
+        payment_transaction.attributes = { gateway_response: { payer_id: payer_id, token: token, transaction_id: response.params["transaction_id"], authorization_id: auth_response.params["transaction_id"]} }
+        payment_transaction.save if payment_transaction.persisted?
+        payment_transaction
       # TODO: There should be a time limit on this
       # TODO: And a retry process around this.
       rescue ::ActiveMerchant::ConnectionError => ex
         # TODO: I18n integration for the message
         raise ::FlexCommerce::PaypalExpress::Exception::ConnectionError.new("Failed authorising transaction due to a connection error.  Original message was #{ex.message}")
+      end
+
+      def do_express_checkout_payment
+        Retry.call(no_of_retries: 5, rescue_errors: ::ActiveMerchant::ConnectionError) {
+          gateway.order(convert_amount(cart.total), token: token, payer_id: payer_id, currency: DEFAULT_CURRENCY)
+        }
+      end
+
+
+      def do_authorization
+        Retry.call(no_of_retries: 5, rescue_errors: ::ActiveMerchant::ConnectionError) {
+          gateway.authorize_transaction(response.params["transaction_id"], convert_amount(cart.total), transaction_entity: "Order", currency: DEFAULT_CURRENCY, payer_id: payer_id)
+        }
       end
     end
   end
