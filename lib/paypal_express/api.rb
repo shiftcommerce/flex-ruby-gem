6a016eeec01ef89b6efccce10e1d5f0c1288195e
Richard Watkins <richardwatkins1@gmail.com>
2019-02-05 11:09:21 +0000

    revert test_mode change

--- lib/paypal_express/api.rb
+++ lib/paypal_express/api.rb
@@ -24,13 +24,13 @@ module FlexCommerce
 
       def verify_credentials
         unless paypal_login.present? && paypal_password.present? && paypal_signature.present? then
-          raise "Please ensure all Paypal Credentails are set in your env file." 
+          raise "Please ensure all Paypal Credentails are set in your env file."
         end
       end
 
       # DEFAULT value for test mode is true.
       def test_mode
-        FlexCommerceApi.config.order_test_mode || true
+        FlexCommerceApi.config.order_test_mode == true
       end
 
       # PAYPAL CREDENTAILS
