4df4ba71473ad21501f5e2f314fa94c08613bd1b
Zeeshan Ishtiaq <zeeshan.ishtiaq@shiftcommerce.com>
2019-09-10 16:25:20 +0100

    Add changes for gift cards

--- lib/paypal_express/auth.rb
+++ lib/paypal_express/auth.rb
@@ -6,14 +6,14 @@ require 'retry'
 module FlexCommerce
   module PaypalExpress
     # @class Setup
-    #  
+    #
     # This service authorises the payment via the Paypal gateway
     class Auth
       include ::Retry
       include ::FlexCommerce::PaypalExpress::Api
-      
+
       DEFAULT_CURRENCY = "GBP"
-      
+
       # @initialize
       #
       # @param {String} token - Paypal token
@@ -29,9 +29,9 @@ module FlexCommerce
       def call
         process_with_gateway
       end
- 
+
       private
-  
+
       attr_accessor :cart, :token, :payer_id, :payment_transaction, :gateway_class
 
       def process_with_gateway
@@ -63,7 +63,7 @@ module FlexCommerce
       def do_express_checkout_payment
         Retry.call(no_of_retries: no_of_retires, rescue_errors: ::ActiveMerchant::ConnectionError) {
           ::NewRelic::Agent.increment_metric('Custom/Paypal/Do_Express_Checkout_Payment') if defined?(NewRelic::Agent)
-          gateway.order(convert_amount(cart.total), token: token, payer_id: payer_id, currency: DEFAULT_CURRENCY)
+          gateway.order(convert_amount(payment_transaction.amount), token: token, payer_id: payer_id, currency: DEFAULT_CURRENCY)
         }
       end
 
@@ -71,7 +71,7 @@ module FlexCommerce
       def do_authorization(response)
         Retry.call(no_of_retries: no_of_retires, rescue_errors: ::ActiveMerchant::ConnectionError) {
           ::NewRelic::Agent.increment_metric('Custom/Paypal/Do_Auhtorization') if defined?(NewRelic::Agent)
-          gateway.authorize_transaction(response.params["transaction_id"], convert_amount(cart.total), transaction_entity: "Order", currency: DEFAULT_CURRENCY, payer_id: payer_id)
+          gateway.authorize_transaction(response.params["transaction_id"], convert_amount(payment_transaction.amount), transaction_entity: "Order", currency: DEFAULT_CURRENCY, payer_id: payer_id)
         }
       end
 
