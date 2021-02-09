622a09c25af940fc532a424a7abe059b396ec587
Zeeshan Ishtiaq <zeeshan.ishtiaq@shiftcommerce.com>
2021-02-09 10:25:46 +0000

    undo changes to remote builder

--- lib/flex_commerce_api/json_api_client_extension/remote_builder.rb
+++ lib/flex_commerce_api/json_api_client_extension/remote_builder.rb
@@ -1,46 +1,24 @@
 module FlexCommerceApi
   module JsonApiClientExtension
     class RemoteBuilder < ::JsonApiClient::Query::Builder
-      def initialize(klass, opts = {}, path: klass.path, connection: klass.connection)
+      def initialize(klass, path: klass.path, connection: klass.connection)
         super(klass)
         self.connection = connection
         self.path = path
-        @primary_key       = opts.fetch(:primary_key, nil)
-        @pagination_params = opts.fetch(:pagination_params, {})
-        @path_params       = opts.fetch(:path_params, {})
-        @additional_params = opts.fetch(:additional_params, {})
-        @filters           = opts.fetch(:filters, {})
-        @includes          = opts.fetch(:includes, [])
-        @orders            = opts.fetch(:orders, [])
-        @fields            = opts.fetch(:fields, [])
       end
 
       def find(args = {})
         case args
           when Hash
-            scope = where(args)
+            where(args)
           else
-            scope = _new_scope(primary_key: args)
+            @primary_key = args
         end
 
-        get_request(scope.params)
+        get_request(params)
       end
 
       private
-
-      def _new_scope( opts = {} )
-        self.class.new(
-          @klass,
-          primary_key:       opts.fetch(:primary_key, @primary_key),
-          pagination_params: @pagination_params.merge(opts.fetch(:pagination_params, {})),
-          path_params:       @path_params.merge(opts.fetch(:path_params, {})),
-          additional_params: @additional_params.merge(opts.fetch(:additional_params, {})),
-          filters:           @filters.merge(opts.fetch(:filters,{})),
-          includes:          @includes + opts.fetch(:includes, []),
-          orders:            @orders + opts.fetch(:orders, []),
-          fields:            @fields + opts.fetch(:fields, [])
-        )
-      end
 
       def get_request(params)
         klass.parser.parse(klass, connection.run(:get, path, params, klass.custom_headers))
